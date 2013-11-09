a3c3bb311e73af5e0142cbdd33f9eaab2fc51748
Eugene Sturm <sturm049@gmail.com>
2013-11-09 13:14:07 -0600

    possible physics changes

--- DirtyGame/Game/Core/Physics.cs
+++ DirtyGame/Game/Core/Physics.cs
@@ -12,97 +12,44 @@ namespace DirtyGame.game.Core
 {
     public class Physics
     {
-        private Dictionary<int, Entity> entityDictionary;
-        private Dictionary<uint, Body> bodyDictionary;
+      
         private FarseerPhysics.Dynamics.World physicsWorld;
 
         public Physics()
         {
             physicsWorld = new FarseerPhysics.Dynamics.World(new Vector2(0, 0));
-            entityDictionary = new Dictionary<int, Entity>();
-            bodyDictionary = new Dictionary<uint, Body>();
+          
         }
 
-        public FarseerPhysics.Dynamics.World World
-        {
-            get
-            {
-                return physicsWorld;
-            }
-        }
-
-        public Dictionary<int, Entity> Entity
-        {
-            get
-            {
-                return entityDictionary;
-            }
-        }
-
-        public Dictionary<uint, Body> Body
-        {
-            get
-            {
-                return bodyDictionary;
-            }
-        }
-
-        public void AddEntity(int key, Entity e)
-        {
-            entityDictionary.Add(key, e);
-        }
-
-        public void RemoveEntity(int key)
-        {
-            if (entityDictionary.ContainsKey(key))
-            {
-                entityDictionary.Remove(key);
-            }
-        }
-
-        public void AddBody(uint key, Body Body)
-        {
-            bodyDictionary.Add(key, Body);
-        }
-
-        public void RemoveBody(uint key)
-        {
-            if (bodyDictionary.ContainsKey(key))
-            {
-                bodyDictionary.Remove(key);
-            }
-        }
+     
 
         public void Update(GameTime gameTime)
         {
             physicsWorld.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
         }
 
-        public List<Entity> Query(Vector2 center, float width, float height)
+        public List<Body> Query(Vector2 center, float width, float height)
         {
-            List<int> Found = new List<int>();
-            List<Entity> entity = new List<Entity>();
+            List<Body> found = new List<Body>();          
             List<Fixture> fixtures = new List<Fixture>();
-
+                            
             FarseerPhysics.Collision.AABB Box = new FarseerPhysics.Collision.AABB(
                 ConvertUnits.ToSimUnits(center),
                 ConvertUnits.ToSimUnits(width),
                 ConvertUnits.ToSimUnits(height));
 
-            fixtures = physicsWorld.QueryAABB(ref Box);
+            fixtures = physicsWorld.QueryAABB(ref Box);          
             
             foreach (Fixture f in fixtures)
             {
-
-                if (!Found.Contains(f.Body.BodyId))
+                if (!found.Contains(f.Body))
                 {
-                    Found.Add(f.Body.BodyId);
-                    entity.Add(entityDictionary[f.Body.BodyId]);
+                    found.Add(f.Body);                    
                 }
             }
 
 
-            return entity;
+            return found;
         }
 
     }
--- DirtyGame/Game/Core/Systems/PhysicsSystem.cs
+++ DirtyGame/Game/Core/Systems/PhysicsSystem.cs
@@ -18,16 +18,34 @@ namespace DirtyGame.game.Core.Systems
 
         private Physics physics;
         private FarseerPhysics.Dynamics.World physicsWorld;
+        private Dictionary<Body, Entity> bodyToEntityDictionary;
+        private Dictionary<Entity, Body> entityToBodyDictionary;
 
         public PhysicsSystem(Physics  physics) 
             : base(SystemDescriptions.PhysicsSystem.Aspect, SystemDescriptions.PhysicsSystem.Priority)
         {
+            bodyToEntityDictionary = new Dictionary<Body, Entity>();
+            entityToBodyDictionary = new Dictionary<Entity, Body>();
             this.physics = physics;
             physicsWorld = physics.World;
 
             ConvertUnits.SetDisplayUnitToSimUnitRatio(64f);
         }
 
+
+        public List<Entity> Query(Vector2 center, float width, float height)
+        {
+            List<Entity> queryEntities = new List<Entity>() ;
+            List<Body> bodies = physics.Query(center, width, height);
+            foreach (Body body in bodies)
+            {
+                if (bodyToEntityDictionary.ContainsKey(body))
+                {
+                    queryEntities.Add(bodyToEntityDictionary[body]);
+                }
+            }
+            return queryEntities;
+        }
 
         public override void ProcessEntities(IEnumerable<Entity> entities, float dt)
         {
@@ -36,21 +54,14 @@ namespace DirtyGame.game.Core.Systems
             {
                 Spatial spatial = e.GetComponent<Spatial>();
                 spatial.Position = ConvertUnits.ToDisplayUnits(physics.Body[e.Id].Position);
-                
-               if(e.HasComponent<MovementComponent>()) //Some could be static
-               {
-                    
+
+                if (e.HasComponent<MovementComponent>()) //Some could be static
+                {
+
                     MovementComponent movement = e.GetComponent<MovementComponent>();
                     physics.Body[e.Id].LinearVelocity = movement.Velocity;
-                
+
                 }
-                
-
-
-                
-             
-                
-
             }
         }
 
@@ -59,14 +70,10 @@ namespace DirtyGame.game.Core.Systems
             Spatial spatial = e.GetComponent<Spatial>();
             Body Body = new Body(physicsWorld);
 
-            Body = BodyFactory.CreateRectangle(physicsWorld, ConvertUnits.ToSimUnits
-                                                                                  (spatial.Width), ConvertUnits.ToSimUnits(spatial.Height), 1f, 
-                                                                                  ConvertUnits.ToSimUnits(spatial.Position));
-
-           
-            
-                Body.BodyType = BodyType.Dynamic;
-                Body.Restitution = 0.3f;
+            Body = BodyFactory.CreateRectangle(physicsWorld, ConvertUnits.ToSimUnits(spatial.Width), ConvertUnits.ToSimUnits(spatial.Height), 1f, ConvertUnits.ToSimUnits(spatial.Position));
+                       
+            Body.BodyType = BodyType.Dynamic;
+            Body.Restitution = 0.3f;
             
 
             if (e.HasComponent<Collidable>())
@@ -75,10 +82,20 @@ namespace DirtyGame.game.Core.Systems
             }
 
             CollisionCategory(e, Body);
-
-            physics.AddEntity(Body.BodyId, e);
-            physics.AddBody(e.Id, Body);
+            bodyToEntityDictionary.Add(Body, e);
+            entityToBodyDictionary.Add(e, Body);
         }
+
+
+        public override void OnEntityRemoved(Entity e)
+        {
+            if (entityToBodyDictionary.ContainsKey(e))
+            {
+                Body b = entityToBodyDictionary[e];
+                physicsWorld.RemoveBody(b);
+            }            
+        }
+
 
         private bool BodyOnCollision(Fixture fixtureA, Fixture fixtureB, FarseerPhysics.Dynamics.Contacts.Contact contact)
         {
@@ -126,20 +143,6 @@ namespace DirtyGame.game.Core.Systems
                 }
             }
             return true;
-        }
-
-        public override void OnEntityRemoved(Entity e)
-        {
-            if(physics.Body.ContainsKey(e.Id)) 
-            {
-                if(physics.Entity.ContainsKey(physics.Body[e.Id].BodyId))
-                {
-                    physics.Body[e.Id].Dispose();
-                    physics.RemoveEntity(physics.Body[e.Id].BodyId);
-                }
-                physics.RemoveBody(e.Id);
-            } 
-           
         }
 
 
