905c7fb9b5312117eee439cf417d547c599605cf
Jared Poetter <poet0007@umn.edu>
2013-12-05 19:47:30 -0600

    Removed commented code.

--- DirtyGame/Game/Core/Systems/Render/AnimationSystem.cs
+++ DirtyGame/Game/Core/Systems/Render/AnimationSystem.cs
@@ -49,8 +49,6 @@ namespace DirtyGame.game.Systems
                 DirectionComponent direction = e.GetComponent<DirectionComponent>();
                 SpatialComponent sp = e.GetComponent<SpatialComponent>();
 
-                //animation.CurrentAnimation = direction.Heading;
-
                 if (animation == null) //This is a weird bug when adding and removing the animation component. It might have to do with priorities of the systems
                 {
                     continue;
@@ -89,16 +87,14 @@ namespace DirtyGame.game.Systems
                             {
                                 animation.StartedFiniteAnimation = false;
                                 animation.FinishedFiniteAnimation = true;
-                                //animation.CurrentAnimation = animation.CurrentAnimation.Remove(0, 6); //this needs to change the currentAnimation to just the direction
-                                //animation.CurrentAnimation = "Down";
+                                //Removing the player's melee entity from the world when the animation is finished
                                 if (e.GetComponent<NameComponent>().Name.Equals("PLAYER MELEE"))
                                 {
                                     entitiesToDelete.Add(e);
                                 }
+                                //Removing the entity's AnimationComponent when the finite animation is finished
                                 else
                                 {
-                                    //animation.CurrentAnimation = "Idle" + direction.Heading;
-
                                     entitiesToRemoveAnimationComponent.Add(e);
                                 }
                             }
@@ -119,37 +115,6 @@ namespace DirtyGame.game.Systems
                         }
                     }
                 }
-
-                //if (sp.isMoving)
-                //{
-                //    //Move the sprite to the next frame
-                //    //Adding to the time since last draw
-                //    animation.TimeElapsed += dt;
-                //    //Saving the time between frames
-                //    double timeBetweenFrames = 1.0f / sprite.SpriteSheet.Animation[animation.CurrentAnimation].Length;
-                //    //Check to see if enough time has passed to render the next frame
-                //    if (animation.TimeElapsed > timeBetweenFrames)
-                //    {
-                //        //Subtracting the time to get ready for the next frame
-                //        animation.TimeElapsed -= timeBetweenFrames;
-                //        //Checking to make sure we are not going over the number of frames
-                //        if (animation.CurrentFrame < (sprite.SpriteSheet.Animation[animation.CurrentAnimation].Length - 1))
-                //        {
-                //            animation.CurrentFrame++;
-                //        }
-                //        //Starting back at frame 0
-                //        else
-                //        {
-                //            animation.CurrentFrame = 0;
-                //        }
-                //    }
-                //}
-                //else
-                //    animation.CurrentFrame = 0;
-
-                if(e.HasComponent<MeleeComponent>() ){
-
-                    }
 
                 //Setting the rectangle of the sprite sheet to draw
                 sprite.SrcRect = sprite.SpriteSheet.Animation[animation.CurrentAnimation][animation.CurrentFrame];
