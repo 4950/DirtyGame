09012230575efc02e943845a3ad2d0f38387b2ae
Conrad Etzel <dawlsc@gmail.com>
2014-01-15 09:08:51 -0600

    fixed SparnerComponent Typo

---
+++ DirtyGame/Game/Core/Components/SpawnerComponent.cs
@@ -0,0 +1,20 @@
+﻿using DirtyGame.game.Core.Components.Render;
+using EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DirtyGame.game.Core.Components
+{
+    public class SpawnerComponent : Component
+    {
+        public TimeSpan timePerSpawn;
+        public int numMobs;
+        public TimeSpan timeOfLastSpawn;
+        [XmlIgnoreAttribute]
+        public SpriteComponent sprite;
+        public MonsterData data;
+    }
+}
