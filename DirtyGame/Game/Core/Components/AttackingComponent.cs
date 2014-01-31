e9e2b3f6438f394c1075ccdf84e6ea6f5807d5d5
Jared Poetter <poet0007@umn.edu>
2014-01-31 12:54:38 -0600

    Added AttackingComponent to store the bool value to let the system know if the player/monster is attacking.

---
+++ DirtyGame/Game/Core/Components/AttackingComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntityFramework;
+
+namespace DirtyGame.game.Core.Components
+{
+    public class AttackingComponent : Component
+    {
+        #region Variables
+        //If entity is attacking
+        private bool attacking = false;
+        #endregion
+
+        #region Properties
+        public bool isAttacking
+        {
+            get
+            {
+                return attacking;
+            }
+            set
+            {
+                attacking = value;
+            }
+        }
+        #endregion
+    }
+}
