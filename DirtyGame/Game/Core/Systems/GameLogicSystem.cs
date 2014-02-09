2a23a780d57ceb8153889a0d6b5743e65002c587
Adeeb Mohammed <moham536@umn.edu>
2014-02-08 18:30:30 -0600

    Undoing bad stuff

--- DirtyGame/Game/Core/Systems/GameLogicSystem.cs
+++ DirtyGame/Game/Core/Systems/GameLogicSystem.cs
@@ -66,7 +66,7 @@ namespace DirtyGame.game.Core.Systems
             roundLblTime = 3f;
 
             int numRanged = 2 + 2 * CurrentLevel;
-            int numMelee = 0;// 2 + 2 * CurrentLevel;
+            int numMelee = 2 + 2 * CurrentLevel;
 
             switch (CurrentLevel)
             {
