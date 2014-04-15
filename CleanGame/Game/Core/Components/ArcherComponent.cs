37d52c9356a4a972c948c050c34e2c18c2e4908d
Conrad Etzel <dawlsc@gmail.com>
2014-04-14 19:01:25 -0500

    Added ArcherComponent

---
+++ CleanGame/Game/Core/Components/ArcherComponent.cs
@@ -0,0 +1,16 @@
+﻿using EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanGame.Game.Core.Components
+{
+    class ArcherComponent : Component
+    {
+        public TimeSpan timeOfWeaponCheck;
+        public TimeSpan timeToNextCheck;
+        public float timeElapsed;
+    }
+}
