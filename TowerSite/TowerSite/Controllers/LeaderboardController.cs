507a2a5ae3fbb6449af8bdf4b8f2ef6b9b51a067
Adeeb Mohammed <moham536@umn.edu>
2014-04-27 23:34:50 -0500

    Leaderboard shows top 1000

--- TowerSite/TowerSite/Controllers/LeaderboardController.cs
+++ TowerSite/TowerSite/Controllers/LeaderboardController.cs
@@ -22,7 +22,7 @@ namespace TowerSite.Controllers
         GO
 
         CREATE VIEW Leaderboard AS
-        SELECT TOP 100 IsNull(ROW_NUMBER() OVER(ORDER BY A.LinearELO DESC), -1) AS Rank, B.UserName, A.ELO, A.LinearELO FROM PlayerELOes A, AspNetUsers B WHERE A.UserID = B.Id ORDER BY LinearELO DESC
+        SELECT TOP 1000 IsNull(ROW_NUMBER() OVER(ORDER BY A.LinearELO DESC), -1) AS Rank, B.UserName, A.ELO, A.LinearELO FROM PlayerELOes A, AspNetUsers B WHERE A.UserID = B.Id AND A.GamesPlayed > 1 ORDER BY LinearELO DESC
         */
 
         private Entities db = new Entities();
